b2c7d24fb5f77eb05019f90db5264ea519412d97
S0PL0 <gianlucaspl12@gmail.com>
2024-12-09 01:01:49 -0600

    Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,21 @@
-export default function Home() {
-  return (
-    <>
-      <section className="pink_container">
-        <h1 className="heading">
-          Pitch Your Startup, <br /> Connect With Entrepreneurs
-        </h1>
+import SearchForm from "@/components/SearchForm";
 
-        <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-          Competitions.
-        </p>
-      </section>
-    </>
-  );
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+    const query = (await searchParams).query;
+
+    return (
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch Your Startup, <br /> Connect With Entrepreneurs
+                </h1>
+
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+                </p>
+
+                <SearchForm query={query} />
+            </section>
+        </>
+    );
 }
