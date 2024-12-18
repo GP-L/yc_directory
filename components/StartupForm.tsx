811b109c785d3c908192b72cefebbe149b8b30a4
S0PL0 <gianlucaspl12@gmail.com>
2024-12-18 03:50:31 -0600

    Fix toast notification background

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -35,7 +35,7 @@ export default function StartupForm() {
       if (result.status === 'SUCCESS') {
         toast({
           title: 'Success',
-          description: 'Your pitch has been submitted',
+          description: 'Your startup has been submitted',
         })
         router.push(`/startup/${result._id}`)
       }
@@ -78,80 +78,80 @@ export default function StartupForm() {
   })
 
   return (
-    <form action={formAction} className="startup-form">
+    <form action={formAction} className='startup-form'>
       <div>
-        <label htmlFor="title" className="startup-form_label">
+        <label htmlFor='title' className='startup-form_label'>
           Title
         </label>
         <Input
-          id="title"
-          name="title"
-          className="startup-form_input"
+          id='title'
+          name='title'
+          className='startup-form_input'
           required
-          placeholder="Startup Title"
+          placeholder='Startup Title'
         />
 
-        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+        {errors.title && <p className='startup-form_error'>{errors.title}</p>}
       </div>
 
       <div>
-        <label htmlFor="description" className="startup-form_label">
+        <label htmlFor='description' className='startup-form_label'>
           Description
         </label>
         <Textarea
-          id="description"
-          name="description"
-          className="startup-form_textarea"
+          id='description'
+          name='description'
+          className='startup-form_textarea'
           required
-          placeholder="Startup Description"
+          placeholder='Startup Description'
         />
 
         {errors.description && (
-          <p className="startup-form_error">{errors.description}</p>
+          <p className='startup-form_error'>{errors.description}</p>
         )}
       </div>
 
       <div>
-        <label htmlFor="category" className="startup-form_label">
+        <label htmlFor='category' className='startup-form_label'>
           Category
         </label>
         <Input
-          id="category"
-          name="category"
-          className="startup-form_input"
+          id='category'
+          name='category'
+          className='startup-form_input'
           required
-          placeholder="Startup Category (Tech, Health, Education...)"
+          placeholder='Startup Category (Tech, Health, Education...)'
         />
 
         {errors.category && (
-          <p className="startup-form_error">{errors.category}</p>
+          <p className='startup-form_error'>{errors.category}</p>
         )}
       </div>
 
       <div>
-        <label htmlFor="link" className="startup-form_label">
+        <label htmlFor='link' className='startup-form_label'>
           Image URL
         </label>
         <Input
-          id="link"
-          name="link"
-          className="startup-form_input"
+          id='link'
+          name='link'
+          className='startup-form_input'
           required
-          placeholder="Startup Image URL"
+          placeholder='Startup Image URL'
         />
 
-        {errors.link && <p className="startup-form_error">{errors.link}</p>}
+        {errors.link && <p className='startup-form_error'>{errors.link}</p>}
       </div>
 
-      <div data-color-mode="light">
-        <label htmlFor="pitch" className="startup-form_label">
+      <div data-color-mode='light'>
+        <label htmlFor='pitch' className='startup-form_label'>
           Pitch
         </label>
         <MDEditor
           value={pitch}
           onChange={value => setPitch(value as string)}
-          id="pitch"
-          preview="edit"
+          id='pitch'
+          preview='edit'
           height={300}
           style={{ borderRadius: 20, overflow: 'hidden' }}
           textareaProps={{
@@ -162,16 +162,16 @@ export default function StartupForm() {
             disallowedElements: ['style'],
           }}
         />
-        {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
+        {errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
       </div>
 
       <Button
-        type="submit"
-        className="startup-form_btn text-white"
+        type='submit'
+        className='startup-form_btn text-white'
         disabled={isPending}
       >
         {isPending ? 'Submitting...' : 'Submit Your Pitch'}
-        <Send className="size-6 ml-2" />
+        <Send className='size-6 ml-2' />
       </Button>
     </form>
   )
